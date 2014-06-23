bbd91f133836b829ab18d846335498fa584849aa
Wade Okumura <theprofessional9@gmail.com>
2014-06-23 00:26:27 -1000

    Drew an X

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame
+import pygame.freetype
 from pygame.locals import *
 
 # Constants
+PLAYER = 'player'
+COMPUTER = 'computer'
 WINDOWWIDTH = 600
 WINDOWHEIGHT= 500
 TILESIZE    = 120
+HALFTILE    = int(TILESIZE / 2)
 BOARDWIDTH  = 3
 BOARDHEIGHT = 3
-MARKERSIZE  = 40
+FONTSIZE = 50
 
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * TILESIZE)) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * TILESIZE)) / 2)
@@ -16,16 +20,37 @@ YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * TILESIZE)) / 2)
 WHITE       = (255, 255, 255)
 DARKGRAY    = (40, 40, 40)
 
+def left_top_coords(tilex, tiley):
+    '''
+    returns left and top pixel coords
+    tilex: int
+    tiley: int
+    return: int, int
+    '''
+    left = tilex * TILESIZE + XMARGIN
+    top = tiley * TILESIZE + YMARGIN
+    return left, top
+
 def main():
     #global WINDOWWIDTH, WINDOWHEIGHT, TILESIZE, BOARDWIDTH, BOARDHEIGHT, MARKERSIZE, XMARGIN, YMARGIN
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode( (WINDOWWIDTH, WINDOWHEIGHT) )
     pygame.display.set_caption("Tic-Tac-Toe")
+    FONT = pygame.freetype.Font(None, FONTSIZE)
     
     board = [[None] * 3 for i in range(3)]
+    # For testing only
+    board[0][0] = PLAYER
     
     while True:
-        # Draw board
+        # Make tile rects
+        for y in range(len(board)):
+            for x in range(len(board[y])):
+                if board[y][x] == PLAYER:
+                    surf, surfrect = FONT.render('X', DARKGRAY, None)
+                    surfrect.center = (int(XMARGIN + (x * TILESIZE) + HALFTILE), int(YMARGIN + (y * TILESIZE) + HALFTILE))
+                    DISPLAYSURF.blit(surf, surfrect)
+        # Draw board lines
         for x in range(TILESIZE, BOARDWIDTH * TILESIZE, TILESIZE):
             pygame.draw.line(DISPLAYSURF, DARKGRAY, 
                (x + XMARGIN, YMARGIN), (x + XMARGIN, WINDOWHEIGHT - YMARGIN), 6)
